c77627506936bef34135376f1b3d4529489d04e8
Catishere <viktor.gyoshev@gmail.com>
2022-03-29 17:17:37 +0300

    update statistics tests

--- VVPSProjectTests/StatisticsUtilsTest/tst_statisticsutilstest.cpp
+++ VVPSProjectTests/StatisticsUtilsTest/tst_statisticsutilstest.cpp
@@ -14,11 +14,16 @@ public:
 
 private slots:
     static void testCalculateMode();
-    static void testCalculateMedian();
+    static void testCalculateModeAmodal();
+    static void testCalculateModeTrimodal();
+    static void testCalculateMedianOdd();
+    static void testCalculateMedianEven();
     static void testCalculateAverage();
     static void testCalculateScope();
-    static void testCalculateDeviation();
-    static void testCalculateVariance();
+    static void testCalculateDeviationSample();
+    static void testCalculateDeviationComplete();
+    static void testCalculateVarianceSample();
+    static void testCalculateVarianceComplete();
 
 };
 
@@ -48,26 +53,36 @@ void StatisticsUtilsTest::testCalculateMode()
     auto mode = stats.calculateMode(testData);
     QCOMPARE(mode.length(), 1);
     QCOMPARE(mode.at(0), 15);
+}
 
-    mode = stats.calculateMode(QList<int>
-                                          { 1, 2, 3, 3, 4, 4, 5, 5});
+void StatisticsUtilsTest::testCalculateModeAmodal()
+{
+    StatisticsUtils stats;
+    auto mode = stats.calculateMode(QList<int> { 1, 2, 3, 4, 5});
+    QCOMPARE(mode.length(), 0);
+}
+
+void StatisticsUtilsTest::testCalculateModeTrimodal()
+{
+    StatisticsUtils stats;
+    auto mode = stats.calculateMode(QList<int> { 1, 2, 3, 3, 4, 4, 5, 5});
     QCOMPARE(mode.length(), 3);
     QCOMPARE(mode.at(0), 3);
     QCOMPARE(mode.at(1), 4);
     QCOMPARE(mode.at(2), 5);
-
-
-    mode = stats.calculateMode(QList<int>
-                                          { 1, 2, 3, 4, 5});
-    QCOMPARE(mode.length(), 0);
 }
 
-void StatisticsUtilsTest::testCalculateMedian()
+void StatisticsUtilsTest::testCalculateMedianOdd()
 {
     StatisticsUtils stats;
     auto median = stats.calculateMedian(testData);
     QCOMPARE(median, 14);
-    median = stats.calculateMedian(testDataEven);
+}
+
+void StatisticsUtilsTest::testCalculateMedianEven()
+{
+    StatisticsUtils stats;
+    auto median = stats.calculateMedian(testDataEven);
     QCOMPARE(median, 14.5);
 }
 
@@ -85,24 +100,36 @@ void StatisticsUtilsTest::testCalculateScope()
     QCOMPARE(scope, 19);
 }
 
-void StatisticsUtilsTest::testCalculateDeviation()
+void StatisticsUtilsTest::testCalculateDeviationSample()
 {
+
     StatisticsUtils stats;
     auto deviation = stats.calculateDeviation(testData);
     QCOMPARE(deviation, 5.23354564325);
+}
+
+void StatisticsUtilsTest::testCalculateDeviationComplete()
+{
+    StatisticsUtils stats;
     stats.setComplete(true);
-    deviation = stats.calculateDeviation(testData);
+    QVERIFY(stats.getComplete());
+    auto deviation = stats.calculateDeviation(testData);
     QCOMPARE(deviation, 5.12780654861);
 }
 
-void StatisticsUtilsTest::testCalculateVariance()
+void StatisticsUtilsTest::testCalculateVarianceSample()
 {
     StatisticsUtils stats;
     auto variance = stats.calculateVariance(testData);
     QCOMPARE(variance, 27.39);
+}
+
+void StatisticsUtilsTest::testCalculateVarianceComplete()
+{
+    StatisticsUtils stats;
     stats.setComplete(true);
     QVERIFY(stats.getComplete());
-    variance = stats.calculateVariance(testData);
+    auto variance = stats.calculateVariance(testData);
     QCOMPARE(variance, 26.2944);
 }
 
